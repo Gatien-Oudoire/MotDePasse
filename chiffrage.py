8310b1cc3b7fee5b4104b4e85ccdfb1992564831
Gatien Oudoire <naguite.ytb@gmail.com>
2020-12-06 17:51:05 +0100

    Ajout de nouveaux caractères

--- chiffrage.py
+++ chiffrage.py
@@ -12,7 +12,7 @@ def genererMotDePasse(taille):
 
 def chiffrer(texte, decalage=4):
     # Decalage ]0;4]
-    caracteres = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z','0','1','2','3','4','5','6','7','8','9','@','#','%','$']
+    caracteres = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z','0','1','2','3','4','5','6','7','8','9','.','@','#','%','$','!','€']
     resultat = ""
     for lettre in texte:
         dansListe = False
@@ -27,7 +27,7 @@ def chiffrer(texte, decalage=4):
     return resultat
 
 def dechiffrer(texte, decalage=4):
-    caracteres = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z','0','1','2','3','4','5','6','7','8','9','@','#','%','$']
+    caracteres = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z','0','1','2','3','4','5','6','7','8','9','.','@','#','%','$','!','€']
     resultat = ""
     for lettre in texte:
         dansListe = False
